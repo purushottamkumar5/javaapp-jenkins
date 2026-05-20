f2e7a4678938732cd7389e470d6bab280de419f6
Purushottam <kumarpuru11111@gmail.com>
2026-05-20 20:28:47 +0530

    Initial commit

---
+++ javaApp/src/main/java/com/deploy/manual/javaApp/controller/test.java
@@ -0,0 +1,21 @@
+package com.deploy.manual.javaApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+
+    @GetMapping("/health")
+    public String getHealth()
+    {
+        return "Health is OK";
+    }
+
+
+
+
+    
+
+
+}
---
+++ javaApp/src/test/java/com/deploy/manual/javaApp/JavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.manual.javaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
